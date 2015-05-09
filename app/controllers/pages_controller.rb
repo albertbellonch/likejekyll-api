77618efb8ec85f067db2045672f3b1e78b9b1154
Albert Bellonch Llargués <albert@itnig.net>
2015-05-09 14:02:02 +0200

    Add spec for not-found page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < BaseController
   def show
-    @page = Page.find_by_digest(params[:id])
-    render json: @page
+    @page = Page.find_by(digest: params[:id])
+
+    if @page.present?
+      render json: @page
+    else
+      render nothing: true, status: 404
+    end
   end
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,17 +2,28 @@ require 'rails_helper'
 
 describe PagesController do
   describe "GET pages#show" do
-    before do
-      @page = create :page, uri: 'http://amazi.ng/post'
-      6.times { create :like, page: @page }
+    context "when it exists" do
+      before do
+        @page = create :page, uri: 'http://amazi.ng/post'
+        6.times { create :like, page: @page }
+      end
+
+      it "should return the correct information" do
+        get "/pages/#{@page.digest}"
+        page_json = json['page']
+
+        expect(response).to be_success
+        expect(page_json['uri']).to eq('http://amazi.ng/post')
+        expect(page_json['likes_count']).to eq(6)
+      end
     end
 
-    it "returns the correct information" do
-      get "/pages/#{@page.digest}"
-      page_json = json['page']
+    context "when it does not exist" do
+      it "should return a 404 error" do
+        get "/pages/its-oa-trap"
 
-      expect(page_json['uri']).to eq('http://amazi.ng/post')
-      expect(page_json['likes_count']).to eq(6)
+        expect(response).to be_not_found
+      end
     end
   end
 end
